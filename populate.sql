82cc62bcf17a6503232def19e4583f60dcfed15c
VadimPusiak788 <54483201+VadimPusiak788@users.noreply.github.com>
2020-04-16 19:44:08 +0300

    Add files via upload

---
+++ populate.sql
@@ -0,0 +1,63 @@
+INSERT INTO player
+(player_name, positions, nationality, ht, wt)
+VALUES
+('Stephen Curry', 'G', 'United States', '6-3', 190);
+
+INSERT INTO selection
+(team, selection_type, draw_status)
+VALUES
+('Golden State Warriors', 'Western All-Star Fan Vote Selection', '2009 Rnd 1 Pick 7');
+
+INSERT INTO teams
+(player_name, team)
+VALUES
+('Stephen Curry', 'Golden State Warriors');
+
+INSERT INTO years
+(player_name, year)
+VALUES
+('Stephen Curry', 2016);
+
+
+INSERT INTO player
+(player_name, positions, nationality, ht, wt)
+VALUES
+('James Harden', 'SG', 'United States', '6-5', 220);
+
+INSERT INTO selection
+(team, selection_type, draw_status)
+VALUES
+('Houston Rockets', 'Western All-Star Fan Vote Selection', '2009 Rnd 1 Pick 3');
+
+INSERT INTO teams
+(player_name, team)
+VALUES
+('James Harden', 'Houston Rockets');
+
+INSERT INTO years
+(player_name, year)
+VALUES
+('James Harden', 2016);
+
+
+
+INSERT INTO player
+(player_name, positions, nationality, ht, wt)
+VALUES
+('Kevin Durant', 'SF', 'United States', '6-9', 240);
+
+INSERT INTO selection
+(team, selection_type, draw_status)
+VALUES
+('Golden State Warriors', 'Western All-Star Fan Vote Selection', '2009 Rnd 1 Pick 2');
+
+INSERT INTO teams
+(player_name, team)
+VALUES
+('Kevin Durant', 'Golden State Warriors');
+
+INSERT INTO years
+(player_name, year)
+VALUES
+('Kevin Durant', 2016);
+
