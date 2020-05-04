5b54d8d151283d065163534c414641ef6fab460b
VadimPusiak788 <54483201+VadimPusiak788@users.noreply.github.com>
2020-05-04 15:12:56 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,10 +1,9 @@
--- Запит №1 Вивести кількість гравців на кожній позиції
+
 select positions, count(player_name) as count_pos
 from player
 GROUP BY positions
 order by count_pos DESC;
 
--- Запити №2 Вивести команди у яких відсоток баскетбольних зірок більший ніж в решти
 select  round((count(player.player_name)/14) * 100, 2) as rate
     --,teams.team
     ,NVL(teams.team, 0) as teams
@@ -14,7 +13,6 @@ on teams.player_name = player.player_name
 GROUP by NVL(teams.team, 0)
 order by teams DESC;
 
--- Запит №3 Вивести динаміку кількості баскетбольних зірок по роках.
 select count(player.player_name) as count_player
             , years.star_years as all_years
             from player
